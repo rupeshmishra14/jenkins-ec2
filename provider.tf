9e5352552116d72674aaae18c4adbbbe6aa71f75
rupesh mishra <rupeshmishra9321@gmail.com>
2023-02-04 21:25:38 +0530

    corrected provider file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  export AWS_ACCESS_KEY_ID="AWS_ACCESS_KEY_ID"
+  export AWS_SECRET_ACCESS_KEY="AWS_SECRET_ACCESS_KEY"
+  export AWS_REGION= "us-east-1"
 }
